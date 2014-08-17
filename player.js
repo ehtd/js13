8d201d516f19c4937fd7d50c35d52680c5e6e4b0
Ernesto Torres <moku.software@gmail.com>
2014-08-16 21:09:02 -0500

    Refactoring into proper game and player js to clean up index

--- player.js
+++ player.js
@@ -4,6 +4,20 @@
 
 //From http://www.p01.org/releases/140bytes_music_softSynth/
 
+//http://countercomplex.blogspot.mx/2011/10/algorithmic-symphonies-from-one-line-of.html
+//(t*5&t>>7)|(t*3&t>>10)
+
+//((t*10)|(t*3 &t >> 15)|(t*30 &t >> 70) | (sin(t)))&255
+//((t*15) | t | t>>7)&255
+
+//	faster implementation in 142bytes
+var softsynth = function(f){return eval("for(var t=0,S='RIFF_oO_WAVEfmt "+atob('EAAAAAEAAQBAHwAAQB8AAAEACAA')+"data';++t<3e5;)S+=String.fromCharCode("+f+")")};
+
+var e,_={};
+_.button 	= document.getElementById('generate');
+_.audio		= document.getElementById('audio');
+_.formula	= document.getElementById('formula');
+_.button.onclick = startPlayer;
 
 function startPlayer() {
     _.button.disabled = true;
